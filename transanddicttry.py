ba3dc25bec11e4f4b2ec6144fe5322b4475b865b
Emre <basarremre@outlook.com>
2022-05-14 06:06:47 +0300

    token is removed from local

--- transanddicttry.py
+++ transanddicttry.py
@@ -46,8 +46,8 @@ logger = logging.getLogger(__name__)
 dictt_answers = wrong_answers_number()
 np.random.shuffle(dictt_answers)
 
-with open('C:/Users/emreb/Documents/projects/secret/token.txt', 'r') as f:
-    TOKEN = f.read()
+# with open('C:/Users/emreb/Documents/projects/secret/token.txt', 'r') as f:
+#     TOKEN = f.read()
 IN, out, tolang, voc, quizans,Quizroutes,quizagain= range(7)
 
 logging.basicConfig(
