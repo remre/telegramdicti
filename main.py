f0c05c3580bc05146bdfec70fa0fe97f9f81ddcb
Emre <basarremre@outlook.com>
2022-05-17 18:55:41 +0300

    webhook

--- main.py
+++ main.py
@@ -130,17 +130,17 @@ def main():
     dispatcher.add_handler(word_quiz)
     dispatcher.add_handler(dictionary_conv)
 
-    # PORT = int(os.environ.get('PORT', '8443'))
-    # updater.start_webhook(listen="0.0.0.0",
-    #                       port=PORT,
-    #                       url_path=TOKEN,
-    #                       webhook_url='https://telegramtrans-app.herokuapp.com/'+TOKEN)
+    PORT = int(os.environ.get('PORT', '8443'))
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN,
+                          webhook_url='https://telegramtrans-app.herokuapp.com/'+TOKEN)
 
     # logging.info(f"Start webhook mode on port PORT:{PORT}")
                         #   webhook_url='https://transanddict.herokuapp.com/'+TOKEN)
                         # to post https://api.telegram.org/bot5390988406:AAGZpy9maBTXPphCxwNdqRjTib3uLCrme4U/setWebhook
 
-    updater.start_polling()
+    # updater.start_polling()
 
     updater.idle()
     # while True:
--- path.py
+++ path.py
@@ -1,25 +1,15 @@
-import requests
+import inflect
 from bs4 import BeautifulSoup as bs
-def dictionary(word):
+from googletrans import Translator
 
-    url_merriam = 'https://www.merriam-webster.com/dictionary/'
-    page = requests.get(url_merriam+ word)
-    soup = bs(page.content, 'html.parser')
-    m = soup.find_all('span', class_='dtText')[:3]
 
-    if m == []:
 
-        words = soup.find_all('p',class_='spelling-suggestions')[:2]
-        dictt =  [w.text for w in words]
-        dictt.append("Try again, The word you've entered isn't in the dictionary maybe some suggestions shown above.")
-    else:
-        try:
-            dictt= [c.text.split(':')[1].strip() for c in m]
-        except:
-            dictt = [c.text.strip() for c in m]
-
-    return (','.join(str(a)for a in dictt))
-
-wordi = dictionary('go away')
-
-print(wordi)
+translator = Translator()
+p = inflect.engine()
+number = (34534,345345,347,312,124534211,24432)
+text  = []
+for i in number:
+    # print(p.number_to_words(i))
+    result = p.number_to_words(i)
+    text = translator.translate(result, dest='es', ).text
+    print(text)
